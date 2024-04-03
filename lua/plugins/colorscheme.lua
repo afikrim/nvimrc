ddddc8766ef48af9650e2d8e48f092ba6b5c984b
Aziz Fikri M <afikrim10@gmail.com>
2024-04-03 17:00:47 +0700

    feat: add colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "shaunsingh/nord.nvim",
+    name = "nord",
+    priority = 1000,
+  },
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000,
+  },
+  {
+    "Mofiqul/vscode.nvim",
+    priority = 1000,
+  },
+
+  -- Configure LazyVim to load catppuccin
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark_dark",
+    },
+  },
+}
