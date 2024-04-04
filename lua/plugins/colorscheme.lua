8af101454d084d4a2322fb9763909100b505fc00
Aziz Fikri M <afikrim10@gmail.com>
2024-04-04 10:50:13 +0700

    feat: update plugins

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,22 +2,66 @@ return {
   {
     "shaunsingh/nord.nvim",
     name = "nord",
-    priority = 1000,
+    lazy = true,
   },
   {
     "olimorris/onedarkpro.nvim",
-    priority = 1000,
+    lazy = true,
+    opts = {},
   },
   {
     "Mofiqul/vscode.nvim",
-    priority = 1000,
+    lazy = true,
+    opts = {},
+  },
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      integrations = {
+        aerial = true,
+        alpha = true,
+        cmp = true,
+        dashboard = true,
+        flash = true,
+        gitsigns = true,
+        headlines = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        leap = true,
+        lsp_trouble = true,
+        mason = true,
+        markdown = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        navic = { enabled = true, custom_bg = "lualine" },
+        neotest = true,
+        neotree = true,
+        noice = true,
+        notify = true,
+        semantic_tokens = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+    },
   },
 
   -- Configure LazyVim to load catppuccin
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "onedark_dark",
+      colorscheme = "catppuccin",
     },
   },
 }
